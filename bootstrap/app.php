414316fabe0645d8ac66f837fb39de7c2f7c87de
madarasz <mr.madarasz.istvan@gmail.com>
2019-05-15 10:06:51 +0200

    CORS enabled for localhost:8000

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -70,6 +70,9 @@ $app->singleton(
 $app->routeMiddleware([
     'jwt.auth' => App\Http\Middleware\JwtMiddleware::class,
 ]);
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+ ]);
 
 /*
 |--------------------------------------------------------------------------
